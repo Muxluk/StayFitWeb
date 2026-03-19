75e358e964fdcedea3d179b78e9b6e6d6d2bff81
Muxluk <mishavashchuk2@gmail.com>
2026-03-20 01:03:40 +0200

    fixing non-null valuables

--- src/StayFit/StayFit.Domain/Entities/MealEntry.cs
+++ src/StayFit/StayFit.Domain/Entities/MealEntry.cs
@@ -3,9 +3,9 @@ namespace StayFit.Domain.Entities;
 public class MealEntry
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public DateTime Time { get; set; }
-    public string UserEmail { get; set; }
+    public string UserEmail { get; set; }  = string.Empty;
 
     public virtual ICollection<FoodLog> FoodLogs { get; set; } = new List<FoodLog>();
 }
