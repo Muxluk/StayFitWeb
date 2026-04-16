c6a44574febd5cdf1286367c66796b9777e92b32
Revadomi <dom.and2005@gmail.com>
2026-04-17 02:08:29 +0300

    Notes_fix

---
+++ src/StayFit/StayFit.Infrastructure/Migrations/20260416230358_AddNoteToFoodLog.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StayFit.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNoteToFoodLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "FoodLogs",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "FoodLogs");
+        }
+    }
+}
